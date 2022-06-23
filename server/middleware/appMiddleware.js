e48cb15bdf791100c3880bb9a990a456a824300f
Salik47 <msalik32@gmail.com>
2022-06-23 18:14:32 +0530

    add cors

--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+const cors = require('cors');
 
 module.exports = (app) => {
   // body parser
   app.use(express.json());
   app.use(cookieParser());
   app.use(morgan("dev"));
+  app.use(cors())
 };
